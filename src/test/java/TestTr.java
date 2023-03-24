007a56a3f0b29cb1c54c7c0d794e7ab77a5a782f
Mikhail Kurbanov <slowwwdowns@gmail.com>
2023-03-24 11:17:03 +0300

    First commiting

---
+++ src/test/java/TestTr.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTr {
+
+        @Test
+        void testTr() {
+
+            Assertions.assertTrue(5 > 1);
+        }
+    }
+
